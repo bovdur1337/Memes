d5a02b0ca02c243871fafbbdc1753f5b412375bd
Olbabej <0lbabej@gmail.com>
2023-05-06 17:50:54 +0200

    favorites fragment (onMemeSwipe)

--- app/src/main/java/com/example/memes/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/example/memes/ui/fragments/FavoritesFragment.kt
@@ -1,17 +1,21 @@
 package com.example.memes.ui.fragments
 
+import android.content.ClipData.Item
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.memes.R
 import com.example.memes.adapter.FavMemesAdapter
 import com.example.memes.databinding.FragmentFavoritesBinding
 import com.example.memes.ui.activity.MainActivity
 import com.example.memes.viewmodel.MemeViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class FavoritesFragment : Fragment() {
 
@@ -41,6 +45,9 @@ class FavoritesFragment : Fragment() {
 
         //observing our livedata with list of fav memes
         observeFavMemes()
+
+        //adding possibility to swipe and delete memes
+        onMemeSwipe()
     }
 
     private fun prepareFavMemesRV(){
@@ -58,5 +65,33 @@ class FavoritesFragment : Fragment() {
         viewModel.favMemesLD.observe(viewLifecycleOwner, Observer { memesList ->
             favMemesAdapter.differ.submitList(memesList)
         })
+    }
+
+    private fun onMemeSwipe(){
+        val itemTouchHelper = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ) = true
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val currentMeme = favMemesAdapter.differ.currentList[position]
+                viewModel.deleteMemeFromFavs(currentMeme)
+
+                Snackbar.make(requireView(), "Meme Deleted", Snackbar.LENGTH_SHORT)
+                    .setAction(
+                        "Undo",
+                        View.OnClickListener {
+                            viewModel.addMemeToFavs(currentMeme)
+                        }
+                    ).show()
+            }
+        }
+        ItemTouchHelper(itemTouchHelper).attachToRecyclerView(binding.rvFavMemes)
     }
 }
